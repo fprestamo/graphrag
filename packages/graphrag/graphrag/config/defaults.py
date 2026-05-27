49fdd83018074188f4ef568882164ec8da8ba9eb
fprestamo <francisco.prestamo.2003@gmail.com>
2026-05-27 08:29:53 -0600

    update

--- packages/graphrag/graphrag/config/defaults.py
+++ packages/graphrag/graphrag/config/defaults.py
@@ -186,7 +186,7 @@ class ExtractGraphNLPDefaults:
 
     normalize_edge_weights: bool = True
     text_analyzer: TextAnalyzerDefaults = field(default_factory=TextAnalyzerDefaults)
-    concurrent_requests: int = 500
+    concurrent_requests: int = 600
     async_mode: AsyncType = AsyncType.Threaded
 
 
@@ -364,7 +364,7 @@ class GraphRagConfigDefaults:
     models: dict = field(default_factory=dict)
     completion_models: dict = field(default_factory=dict)
     embedding_models: dict = field(default_factory=dict)
-    concurrent_requests: int = 500
+    concurrent_requests: int = 600
     async_mode: AsyncType = AsyncType.Threaded
     reporting: ReportingDefaults = field(default_factory=ReportingDefaults)
     input_storage: InputStorageDefaults = field(default_factory=InputStorageDefaults)
